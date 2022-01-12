9b77fd4e29bf42fb7996cec1917408c5ba04388e
Lobna_Ali <lobna_ali@live.com>
2022-01-12 23:53:40 +0200

    add null constrain

--- migrations/sqls/20220112200519-product-table-up.sql
+++ migrations/sqls/20220112200519-product-table-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-CREATE TABLE product (id SERIAL PRIMARY KEY, name VARCHAR(100), price NUMERIC, category VARCHAR(100));
+CREATE TABLE product (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, price NUMERIC(15, 2), category VARCHAR(100));
--- migrations/sqls/20220112200633-users-table-up.sql
+++ migrations/sqls/20220112200633-users-table-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-CREATE TABLE "user" (id SERIAL PRIMARY KEY, firstName VARCHAR(100), lastName VARCHAR(100), password CHAR(60));
+CREATE TABLE "user" (id SERIAL PRIMARY KEY, firstName VARCHAR(100) NOT NULL, lastName VARCHAR(100) NOT NULL, password CHAR(60) NOT NULL);
